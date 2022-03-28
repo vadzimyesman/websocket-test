53aded9ee255a8816db47c4d44b593e1cdbd0fb9
Vadzim_Yesman <vvyesman@gmail.com>
2022-03-28 16:50:24 -0700

    fix6

--- src/components/ChatTest.jsx
+++ src/components/ChatTest.jsx
@@ -11,10 +11,9 @@ const { Meta } = Card;
 
 const client = new W3CWebSocket(`ws://127.0.0.1:4000`);
 
-function ChatTest() {
+function ChatTest(props) {
 
-    const [userName, setUserName]=useState("")
-    const [isLoggedIn, setIsLoggedIn]=useState(false)
+    const [userName, setUserName]=useState(props.nickname)
     const [messages, setMessages]=useState([])
     const [searchVal, setSearchVal]=useState("")
 
@@ -22,7 +21,7 @@ function ChatTest() {
         client.send(JSON.stringify({
           type: "message",
           msg: value,
-          user: userName
+          user: props.nickname
         }));
         setSearchVal("")
       }
@@ -45,23 +44,10 @@ function ChatTest() {
 
   return (
     <div className="main" id='wrapper'>
-    {isLoggedIn ?
+  
     <div>
       <div className="title">
-        <Text id="main-heading" type="secondary" style={{ fontSize: '36px' }}>Websocket Chat: {userName}</Text>
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', paddingBottom: 50 }} id="messages">
-        {messages.map(message => 
-          <Card key={message.msg} style={{ width: 300, margin: '16px 4px 0 4px', alignSelf: userName === message.user ? 'flex-end' : 'flex-start' }} loading={false}>
-            <Meta
-              avatar={
-                <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>{message.user[0].toUpperCase()}</Avatar>
-              }
-              title={message.user+":"}
-              description={message.msg}
-            />
-          </Card> 
-        )}
+        <Text id="main-heading" type="secondary" style={{ fontSize: '36px' }}>Game chat: {props.nickname}</Text>
       </div>
       <div className="bottom">
         <Search
@@ -73,20 +59,22 @@ function ChatTest() {
           onSearch={value => onButtonClicked(value)}
         />
       </div> 
+      <div style={{ display: 'flex', flexDirection: 'column', paddingBottom: 50 }} id="messages">
+        {messages.map((message,index) => 
+          <Card key={index} style={{ width: 300, margin: '16px 4px 0 4px', alignSelf: userName === message.user ? 'flex-end' : 'flex-start' }} loading={false}>
+            <Meta
+              avatar={
+                <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>{message.user[0].toUpperCase()}</Avatar>
+              }
+              title={message.user+":"}
+              description={message.msg}
+            />
+          </Card> 
+        )}
+      </div>
     </div>
-    :
-    <div style={{ padding: '200px 40px' }}>
-      <Search
-        placeholder="Enter Username"
-        enterButton="Login"
-        size="large"
-        onSearch={value =>{
-            setIsLoggedIn(true)
-            setUserName(value)
-        }}
-      />
-    </div>
-  }
+
+  
   </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
